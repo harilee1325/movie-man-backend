7fe21bf7cad59e86ec807814f4c0b5e7a5e3b764
harilee1325 <leehari007@gmail.com>
2019-10-04 18:30:19 +0530

    live 3

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 # route to clear collections
 @app.route("/")
 def hello():
-    db.user_profile_movie_man.remove()
+   # db.user_profile_movie_man.remove()
     return "Welcome to Movie Man!"
 
 
